8a01c14e8e58371ec2d1bfa32bcad984243dd94f
wolfhergang <ferlopezcardenas@gmail.com>
2015-05-19 19:30:06 -0600

    agregado edicion de cheques

---
+++ client/templates/cheques/cheque_editar.js
@@ -0,0 +1,52 @@
+Template.chequeNuevo.helpers({
+	fechaPagoFormateada: function (){
+		var fecha = formatFecha(this.fechaPago);
+		return fecha;
+	}
+});
+
+Template.chequeNuevo.rendered = function () {
+	$('#fechaPago').datepicker({
+	    format: "dd/mm/yyyy",
+	    language: "es",
+	    todayHighlight: true,
+	    autoclose: true
+	});
+};
+
+Template.chequeNuevo.events({
+	'submit form': function (form, tmplt) {
+		form.preventDefault();
+		var formul = form.target,
+			fp = formul.fechaPago.value.split("/"),
+			pagarse = new Date(fp[2],parseInt(fp[1], 10)-1,fp[0]),
+			propiedades = {
+				fechaPago : pagarse,
+				monto : formul.monto.value,
+				proveedor : formul.proveedor.value,
+				numero :formul.numero.value
+			};
+
+		Meteor.call("cheque.editar", cheque, propiedades);
+		Router.go("chequesList");
+	}
+});
+
+formatFecha = function(fecha){
+	var dia = fecha.getDate(),
+		mes = parseInt(fecha.getMonth(), 10) + 1,
+		año = fecha.getFullYear();
+		if (mes<10) {
+			mes="0"+mes;
+		}
+		fechaC = dia+"/"+mes+"/"+año;
+		return fechaC;
+}
+
+function stopRKey(evt) {
+  var evt = (evt) ? evt : ((event) ? event : null);
+  var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+  if ((evt.keyCode == 13) && (node.type=="text"))  {return false;}
+}
+
+document.onkeypress = stopRKey; 
